01242dd2630241f7a6778c2225b9461c58fa1a71
m-klecka <118010822+m-klecka@users.noreply.github.com>
2024-12-02 13:34:07 +0100

    Probably final edits, added remaining charge time calculation

--- src/main.cpp
+++ src/main.cpp
@@ -41,19 +41,24 @@
 #define BMS_SERIAL_RX_PIN 15
 #define BMS_SERIAL_INVERT false
 
+#define DISPLAY_UPDATE_TIME 10		// In seconds
+
 // Serial connections
 HardwareSerial smartBmsSerial(BMS_SERIAL_PERIPHERAL);
 SmartBmsReader smartBmsReader(&smartBmsSerial);
 
 // Define the display
-GxEPD2_BW<GxEPD2_290_GDEY029T71H, GxEPD2_290_GDEY029T71H::HEIGHT> display(GxEPD2_290_GDEY029T71H(/*CS=5*/ SS, /*DC=*/ 17, /*RST=*/ 16, /*BUSY=*/ 4));  // ESPink-Shelf-2.9 GDEY029T94  128x296, SSD1680, (FPC-A005 20.06.15)
+GxEPD2_BW<GxEPD2_290_GDEY029T71H, GxEPD2_290_GDEY029T71H::HEIGHT> display(GxEPD2_290_GDEY029T71H(/*CS=5*/ SS, /*DC=*/17, /*RST=*/16, /*BUSY=*/4)); // ESPink-Shelf-2.9 GDEY029T94  128x296, SSD1680, (FPC-A005 20.06.15)
 
 // Pin definitions for the display
 #define DISPLAY_POWER_PIN 2
 
 // Update interval
-const unsigned long updateInterval = 10000; // Update every 10 seconds (in ms)
+const unsigned long updateInterval = DISPLAY_UPDATE_TIME*1000;
 unsigned long lastUpdateTime = 0;
+
+// Remaining charge time
+float remainingChargeTime = 0.0;
 
 /**
  * @brief Setup.
@@ -61,14 +66,15 @@ unsigned long lastUpdateTime = 0;
 void setup()
 {
 	// Initialize the serial connections
-	Serial.begin(PC_SERIAL_BAUD);
-	smartBmsSerial.begin(BMS_SERIAL_BAUD_RATE, BMS_SERIAL_MODE, BMS_SERIAL_RX_PIN, -1, BMS_SERIAL_INVERT);
+	Serial.begin(PC_SERIAL_BAUD);																				// Begin pc serial monitor
+	smartBmsSerial.begin(BMS_SERIAL_BAUD_RATE, BMS_SERIAL_MODE, BMS_SERIAL_RX_PIN, -1, BMS_SERIAL_INVERT);		// Begin BMS serial
+
 	// Activate the display
-    pinMode(DISPLAY_POWER_PIN, OUTPUT);
-    digitalWrite(DISPLAY_POWER_PIN, HIGH); // Activate the display
-    delay(100); // Wait for the display to initialize
-    display.init(115200); // Initialize the display with the specified baud rate
-    display.setRotation(1); // Rotate the display 90 degrees clockwise
+	pinMode(DISPLAY_POWER_PIN, OUTPUT);																			// Set display pin mode
+	digitalWrite(DISPLAY_POWER_PIN, HIGH); 																		// Activate the display
+	delay(100);							   																		// Wait for the display to initialize
+	display.init(115200);				  																		// Initialize the display with the specified baud rate
+	display.setRotation(1);				 																		// Rotate the display 90 degrees clockwise
 }
 
 /**
@@ -83,6 +89,7 @@ void loop()
 		SmartBmsData smartBmsData;
 		const SmartBmsError err = smartBmsReader.decodeBmsData(&smartBmsData);
 		if (err == SmartBmsError::SBMS_OK)
+
 		{
 			// Data is ok, lets print it
 			Serial.println();
@@ -116,6 +123,21 @@ void loop()
 			Serial.println("===========================");
 			Serial.println();
 
+			// Calculate remaining charge time if charging
+			if (smartBmsData.getPackChargeCurrent() > 5 && smartBmsData.getPackSoc() < 100)
+			{
+				remainingChargeTime = (smartBmsData.getPackCapacity() - smartBmsData.getPackRemainingEnergy()) * 1000 /
+									  (smartBmsData.getPackChargeCurrent() * smartBmsData.getPackVoltage());
+			}
+			else if (smartBmsData.getPackSoc() >= 100)
+			{
+				remainingChargeTime = 0; // Battery already at 100%
+			}
+			else
+			{
+				remainingChargeTime = -1; // Error or invalid value
+			}
+
 			// Update display if enough time has passed
 			unsigned long currentMillis = millis();
 			if (currentMillis - lastUpdateTime >= updateInterval)
@@ -133,78 +155,123 @@ void loop()
 				display.drawBitmap(160, 15, icon_discharge, 24, 24, GxEPD_BLACK);
 				display.drawBitmap(160, 50, icon_down, 24, 24, GxEPD_BLACK);
 				display.drawBitmap(160, 85, icon_cold, 24, 24, GxEPD_BLACK);
-				// display.drawBitmap(320, 15, icon_battery, 45, 75, GxEPD_BLACK);
 
 				// Battery icon dependenat on SoC and charging current
-
-				if (smartBmsData.getPackSoc() < 10) {
-					if (smartBmsData.getPackChargeCurrent() > 0) {
+				if (smartBmsData.getPackSoc() < 10)
+				{
+					if (smartBmsData.getPackChargeCurrent() > 0)
+					{
 						display.drawBitmap(320, 15, icon_battery_0_charging, 45, 75, GxEPD_BLACK);
-					} else {
+					}
+					else
+					{
 						display.drawBitmap(320, 15, icon_battery_0, 45, 75, GxEPD_BLACK);
 					}
-				} else if (smartBmsData.getPackSoc() < 20) {
-					if (smartBmsData.getPackChargeCurrent() > 0) {
+				}
+				else if (smartBmsData.getPackSoc() < 20)
+				{
+					if (smartBmsData.getPackChargeCurrent() > 0)
+					{
 						display.drawBitmap(320, 15, icon_battery_10_charging, 45, 75, GxEPD_BLACK);
-					} else {
+					}
+					else
+					{
 						display.drawBitmap(320, 15, icon_battery_10, 45, 75, GxEPD_BLACK);
 					}
-				} else if (smartBmsData.getPackSoc() < 30) {
-					if (smartBmsData.getPackChargeCurrent() > 0) {
+				}
+				else if (smartBmsData.getPackSoc() < 30)
+				{
+					if (smartBmsData.getPackChargeCurrent() > 0)
+					{
 						display.drawBitmap(320, 15, icon_battery_20_charging, 45, 75, GxEPD_BLACK);
-					} else {
+					}
+					else
+					{
 						display.drawBitmap(320, 15, icon_battery_20, 45, 75, GxEPD_BLACK);
 					}
-				} else if (smartBmsData.getPackSoc() < 40) {
-					if (smartBmsData.getPackChargeCurrent() > 0) {
+				}
+				else if (smartBmsData.getPackSoc() < 40)
+				{
+					if (smartBmsData.getPackChargeCurrent() > 0)
+					{
 						display.drawBitmap(320, 15, icon_battery_30_charging, 45, 75, GxEPD_BLACK);
-					} else {
+					}
+					else
+					{
 						display.drawBitmap(320, 15, icon_battery_30, 45, 75, GxEPD_BLACK);
 					}
-				} else if (smartBmsData.getPackSoc() < 50) {
-					if (smartBmsData.getPackChargeCurrent() > 0) {
+				}
+				else if (smartBmsData.getPackSoc() < 50)
+				{
+					if (smartBmsData.getPackChargeCurrent() > 0)
+					{
 						display.drawBitmap(320, 15, icon_battery_40_charging, 45, 75, GxEPD_BLACK);
-					} else {
+					}
+					else
+					{
 						display.drawBitmap(320, 15, icon_battery_40, 45, 75, GxEPD_BLACK);
 					}
-				} else if (smartBmsData.getPackSoc() < 60) {
-					if (smartBmsData.getPackChargeCurrent() > 0) {
+				}
+				else if (smartBmsData.getPackSoc() < 60)
+				{
+					if (smartBmsData.getPackChargeCurrent() > 0)
+					{
 						display.drawBitmap(320, 15, icon_battery_50_charging, 45, 75, GxEPD_BLACK);
-					} else {
+					}
+					else
+					{
 						display.drawBitmap(320, 15, icon_battery_50, 45, 75, GxEPD_BLACK);
 					}
-				} else if (smartBmsData.getPackSoc() < 70) {
-					if (smartBmsData.getPackChargeCurrent() > 0) {
+				}
+				else if (smartBmsData.getPackSoc() < 70)
+				{
+					if (smartBmsData.getPackChargeCurrent() > 0)
+					{
 						display.drawBitmap(320, 15, icon_battery_60_charging, 45, 75, GxEPD_BLACK);
-					} else {
+					}
+					else
+					{
 						display.drawBitmap(320, 15, icon_battery_60, 45, 75, GxEPD_BLACK);
 					}
-				} else if (smartBmsData.getPackSoc() < 80) {
-					if (smartBmsData.getPackChargeCurrent() > 0) {
+				}
+				else if (smartBmsData.getPackSoc() < 80)
+				{
+					if (smartBmsData.getPackChargeCurrent() > 0)
+					{
 						display.drawBitmap(320, 15, icon_battery_70_charging, 45, 75, GxEPD_BLACK);
-					} else {
+					}
+					else
+					{
 						display.drawBitmap(320, 15, icon_battery_70, 45, 75, GxEPD_BLACK);
 					}
-				} else if (smartBmsData.getPackSoc() < 90) {
-					if (smartBmsData.getPackChargeCurrent() > 0) {
+				}
+				else if (smartBmsData.getPackSoc() < 90)
+				{
+					if (smartBmsData.getPackChargeCurrent() > 0)
+					{
 						display.drawBitmap(320, 15, icon_battery_80_charging, 45, 75, GxEPD_BLACK);
-					} else {
+					}
+					else
+					{
 						display.drawBitmap(320, 15, icon_battery_80, 45, 75, GxEPD_BLACK);
 					}
-				} else if (smartBmsData.getPackSoc() < 100) {
-					if (smartBmsData.getPackChargeCurrent() > 0) {
+				}
+				else if (smartBmsData.getPackSoc() < 100)
+				{
+					if (smartBmsData.getPackChargeCurrent() > 5)
+					{
 						display.drawBitmap(320, 15, icon_battery_90_charging, 45, 75, GxEPD_BLACK);
-					} else {
+					}
+					else
+					{
 						display.drawBitmap(320, 15, icon_battery_90, 45, 75, GxEPD_BLACK);
 					}
-				} else {  // Pokud je hodnota PackSoc přesně 100
+				}
+				else
+				{ // If SOC is already at 100%
 					display.drawBitmap(320, 15, icon_battery_100, 45, 75, GxEPD_BLACK);
 				}
 
-
-				
-				// Text x+34
-				// Text y+17
 				display.setFont(&SourceSans3_Bold9pt7b);
 
 				display.setCursor(49, 33); // Adjust cursor position as needed
@@ -233,33 +300,48 @@ void loop()
 				display.setCursor(320, 115); // Adjust cursor position as needed
 				display.println((String)smartBmsData.getPackSoc() + "%");
 
-				display.setFont(&SourceSans3_Regular9pt7b);
-				
-				display.setCursor(15, 135); // Adjust cursor position as needed
-				display.println((String) "Povoleno: " + (smartBmsData.isAllowedToCharge() ? "Nab:ano" : "Nab:ne") + " " + (smartBmsData.isAllowedToDischarge() ? "Vyb:ano" : "Vyb:ne"));
+				display.setFont(&SourceSans3_Bold9pt7b);
 
-				display.setCursor(15, 155); // Adjust cursor position as needed
-				display.println((String) "Chyba komunikace: " + (smartBmsData.hasCommunicationError() ? "Ano" : "Ne"));
-				
-				display.setCursor(15, 155); // Adjust cursor position as needed
-				// display.print("Alarm Min / Max Voltage: " + String(battery.alarmMinVoltage ? "Active" : "Inactive") + String(battery.alarmMaxVoltage ? "Active" : "Inactive") + "\n");
-				// // display.print("Alarm Max Voltage: " + String(battery.alarmMaxVoltage ? "Active" : "Inactive") + "\n");
-				// display.print("Alarm Min / Max Temp: " + String(battery.alarmMinTemperature ? "Active" : "Inactive") + String(battery.alarmMaxTemperature ? "Active" : "Inactive") + "\n");
-				// // display.print("Alarm Max Temp: " + String(battery.alarmMaxTemperature ? "Active" : "Inactive") + "\n");
+				display.setCursor(15, 135); // Adjust cursor position as needed
+
+				// If communication error print it else show relevant data
+				if (smartBmsData.hasCommunicationError())
+				{
+					display.println("CHYBA KOMUNIKACE");
+				}
+				else if (!smartBmsData.isAllowedToCharge() && (!smartBmsData.isAllowedToDischarge()))
+				{
+					display.println("Vybijeni ZAKAZANO - chyba");
+					display.setCursor(15, 155); // Adjust cursor position as needed
+					display.println("Nabijeni ZAKAZANO - chyba");
+				}
+				else if (!smartBmsData.isAllowedToCharge())
+				{
+					display.println("Nabijeni ZAKAZANO - chyba");
+				}
+				else if (!smartBmsData.isAllowedToDischarge())
+				{
+					display.println("Vybijeni ZAKAZANO - chyba");
+				}
+				else if (remainingChargeTime > 0)
+				{
+					int hours = (int)remainingChargeTime;
+					int minutes = (int)((remainingChargeTime - hours) * 60);
+					display.setCursor(15, 155); // Adjust cursor position as needed
+					display.println((String) "Cas do nabiti: ~" + hours + "h " + minutes + "min");
+				}
 
 				// Display the content
 				display.display();
 			}
-
-
 		}
 		else if (err == SmartBmsError::SBMS_ERR_READ_STREAM)
 		{
 			// Failed to read the input stream
 			Serial.println("Error: Failed to read BMS data. The input stream could not be read.");
-				
+
 			// Clear the display
-			display.fillScreen(GxEPD_WHITE); 
+			display.fillScreen(GxEPD_WHITE);
 			display.setCursor(82, 93); // Adjust cursor position as needed
 			display.setTextColor(GxEPD_BLACK);
 			display.setFont(&SourceSans3_Bold18pt7b);
@@ -275,7 +357,7 @@ void loop()
 			Serial.println("Error: Failed to read BMS data. The checksum is invalid.");
 
 			// Clear the display
-			display.fillScreen(GxEPD_WHITE); 
+			display.fillScreen(GxEPD_WHITE);
 			display.setCursor(52, 93); // Adjust cursor position as needed
 			display.setTextColor(GxEPD_BLACK);
 			display.setFont(&SourceSans3_Bold18pt7b);
@@ -290,5 +372,4 @@ void loop()
 	/*
 	 * Do something else in the meantime, but make sure your serial buffer will not overflow.
 	 */
-
 }
